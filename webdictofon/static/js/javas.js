76d0a8ab9bea53500c58b3e7c5a2215bf4ddf7c5
Pipi <mitj88@gmail.com>
2022-12-07 14:06:46 +0300

    adsad

--- webdictofon/static/js/javas.js
+++ webdictofon/static/js/javas.js
@@ -1,29 +1,3 @@
-// navigator.mediaDevices.getUserMedia({ audio: true})
-//     .then(stream => {
-//         const mediaRecorder = new MediaRecorder(stream)});
-
-
-// navigator.mediaDevices.getUserMedia({ audio: true})
-// .then(stream => {
-//     const mediaRecorder = new MediaRecorder(stream);
-//     let voice = [];
-//     document.querySelector('#start').addEventListener('click', function(){
-//         mediaRecorder.start();
-//     });
-
-//     mediaRecorder.addEventListener("dataavailable",function(event) {
-//         voice.push(event.data);
-//     });
-
-//     document.querySelector('#stop').addEventListener('click', function(){
-//         mediaRecorder.stop();
-//     });
-// });
-// mediaRecorder.addEventListener("stop", function() {
-//     const voiceBlob = new Blob(voice, {type: 'audio/wav'});
-// })
-
-
 var i = 0;
 function change_microphone(){
     if ( i == 0) {
