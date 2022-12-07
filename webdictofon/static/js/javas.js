ff00d237a681d101b57ce91fd1b65b58999943d8
Pipi <mitj88@gmail.com>
2022-12-07 14:38:16 +0300

    adsad

--- webdictofon/static/js/javas.js
+++ webdictofon/static/js/javas.js
@@ -2,12 +2,12 @@ var i = 0;
 function change_microphone(){
     if ( i == 0) {
         document.getElementById("button_microphone1").src="././static/img/stop_microphone.png";
-        document.getElementById("button_microphone1").setAttribute('style', 'width: 60%; height: 60%;')
+        document.getElementById("button_microphone1").setAttribute('style', 'width: 60%; height: 60%;');
         i++;
     }
     else {
         document.getElementById("button_microphone1").src="././static/img/microphone.png";
-        document.getElementById("button_microphone1").setAttribute('style', 'width: 90%; height: 90%;')
+        document.getElementById("button_microphone1").setAttribute('style', 'width: 90%; height: 90%;');
         i--;
     }
 };
