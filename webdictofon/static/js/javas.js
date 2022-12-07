2b28b12b314844d554d52a3f9b5c938d09a17fbd
Pipi <mitj88@gmail.com>
2022-12-07 13:44:17 +0300

    asdas

--- webdictofon/static/js/javas.js
+++ webdictofon/static/js/javas.js
@@ -29,13 +29,11 @@ function change_microphone(){
     if ( i == 0) {
         document.getElementById("button_microphone1").src="././static/img/stop_microphone.png";
         document.getElementById("button_microphone1").setAttribute('style', 'width: 60%; height: 60%;')
-        mediaRecorder.start()
         i++;
     }
     else {
         document.getElementById("button_microphone1").src="././static/img/microphone.png";
         document.getElementById("button_microphone1").setAttribute('style', 'width: 90%; height: 90%;')
-        mediaRecorder.stop()
         i--;
     }
 };
